7c593346e5166463ead3c7bf8025f82dbc239237
Feridun Mert Celebi <mertcelebi93@gmail.com>
2019-02-04 11:17:01 -0800

    Deploy new contracts

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const VeilEther = require("./build/contracts/VeilEther.json");
 const VirtualAugurShare = require("./build/contracts/VirtualAugurShare.json");
+const OwnableVirtualAugurShare = require("./build/contracts/OwnableVirtualAugurShare.json");
 const VirtualAugurShareFactory = require("./build/contracts/VirtualAugurShareFactory.json");
+const OwnableVirtualAugurShareFactory = require("./build/contracts/OwnableVirtualAugurShareFactory.json");
 const VeilCompleteSets = require("./build/contracts/VeilCompleteSets.json");
 
 module.exports.VeilEther = VeilEther;
 module.exports.VirtualAugurShare = VirtualAugurShare;
+module.exports.OwnableVirtualAugurShare = OwnableVirtualAugurShare;
 module.exports.VirtualAugurShareFactory = VirtualAugurShareFactory;
+module.exports.OwnableVirtualAugurShareFactory = OwnableVirtualAugurShareFactory;
 module.exports.VeilCompleteSets = VeilCompleteSets;
