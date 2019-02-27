74475b6a60e9220d18805fa913a15096499740ad
Feridun Mert Celebi <mertcelebi93@gmail.com>
2019-02-27 14:01:52 -0800

    Remove OldVAS

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const VeilEther = require("./build/contracts/VeilEther.json");
 const VirtualAugurShare = require("./build/contracts/VirtualAugurShare.json");
-const OldVirtualAugurShare = require("./build/contracts/OldVirtualAugurShare.json");
 const VirtualAugurShareFactory = require("./build/contracts/VirtualAugurShareFactory.json");
-const OldVirtualAugurShareFactory = require("./build/contracts/OldVirtualAugurShareFactory.json");
 const VeilCompleteSets = require("./build/contracts/VeilCompleteSets.json");
 
 module.exports.VeilEther = VeilEther;
 module.exports.VirtualAugurShare = VirtualAugurShare;
-module.exports.OldVirtualAugurShare = OldVirtualAugurShare;
 module.exports.VirtualAugurShareFactory = VirtualAugurShareFactory;
-module.exports.OldVirtualAugurShareFactory = OldVirtualAugurShareFactory;
 module.exports.VeilCompleteSets = VeilCompleteSets;
