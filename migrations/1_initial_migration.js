7184d1bb8228d9a80c3e85896b00c0c8c43cf83f
grandwizard420 <sluttmuffinz420@gmail.com>
2019-03-31 19:32:47 -0700

    prep for cloneFactory vanity address optimization

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer, _, accounts) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations, {from: "0x5680ca2e3b4f8bc043aceb31837c89dba8bfba75"});
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 const VeilEther = artifacts.require("VeilEther");
 const VirtualAugurShareFactory = artifacts.require("VirtualAugurShareFactory");
 const VeilCompleteSets = artifacts.require("VeilCompleteSets");
+const VirtualAugurShare = artifacts.require("VirtualAugurShare")
 
 module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
-    await deployer.deploy(VeilEther);
-    await deployer.deploy(VirtualAugurShareFactory);
-    await deployer.deploy(VeilCompleteSets);
+    await deployer.deploy(VirtualAugurShare, {from: "0xb142bee56c35df2906f013edfffe901c0b2502b9"});//vanity contract address
+    await deployer.deploy(VeilEther, {from: "0x5680ca2e3b4f8bc043aceb31837c89dba8bfba75"});
+    await deployer.deploy(VirtualAugurShareFactory, {from: "0x5680ca2e3b4f8bc043aceb31837c89dba8bfba75"});
+    await deployer.deploy(VeilCompleteSets, {from: "0x5680ca2e3b4f8bc043aceb31837c89dba8bfba75"});
   });
 };
